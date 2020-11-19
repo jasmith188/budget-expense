c45284e60194dfff07c02a1ea381fde584a72b54
James Smith <jasmith.188@gmail.com>
2020-11-19 18:12:05 -0500

    add login page

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-
 import React from 'react';
-import { SafeAreaView } from 'react-native'
-import HomePage from './HomePage'
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  
-
   return (
-    <SafeAreaView>
-      <HomePage />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' >
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Login" component={LoginPage} options={{
+          title: 'Sign in or Sign up'
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-
 
 export default App;
--- HomePage.js
+++ HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  SafeAreaView,
+    SafeAreaView,
   StyleSheet,
   View,
   Text,
@@ -12,32 +12,32 @@ import Todo from './Todo';
 import Image from './assets/yellow-notebook.jpg';
 
 const styles = StyleSheet.create({
-    titleText: {
-      paddingTop: 80,
-      fontSize: 50,
-      margin: 20,
-      textAlign: 'center',
-      // fontFamily: 'lucida grande',
-    },
-    todoInput: {
-      margin: 20,
-      height: 40,
-      borderColor: 'black',
-      borderWidth: 2,
-    },
-  
-    todoText: {
-      textAlign: 'center',
-      // fontFamily: 'lucida grande',
-    },
-    image: {
-      flex: 1,
-      resizeMode: 'cover',
-      justifyContent: 'center',
-    },
-  });
+  titleText: {
+    paddingTop: 40,
+    fontSize: 50,
+    margin: 20,
+    textAlign: 'center',
+    // fontFamily: 'lucida grande',
+  },
+  todoInput: {
+    margin: 20,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
 
-const HomePage = () => {
+  todoText: {
+    textAlign: 'center',
+    // fontFamily: 'lucida grande',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+});
+
+const HomePage = ({ navigation }) => {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
 
@@ -50,31 +50,37 @@ const HomePage = () => {
     uri:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-notebook-paper-background-design-template-d7dfa26f8c8c759652e2521f0ecd24eb_screen.jpg?ts=1567891546',
   };
-  return <div><SafeAreaView>
-  {/* <ImageBackground source={image} style={styles.image}> */}
-    <View>
-      
-      <Text style={styles.titleText}>Gr🍊cery List</Text>
-    </View>
-
-    <TextInput
-      style={styles.todoInput}
-      value={input}
-      onChangeText={(text) => setInput(text)}
-    />
-    <Button
-      style={styles.appButtonText}
-      color="red"
-      title="Add To List"
-      onPress={addTodo}
-    />
-    <View>
-      {todos.map((todo) => (
-        <Todo style={styles.todoText} title={todo} />
-      ))}
-    </View>
-    {/* </ImageBackground> */}
-</SafeAreaView></div>;
+  return (
+    
+      <SafeAreaView>
+        {/* <ImageBackground source={image} style={styles.image}> */}
+        <View>
+          <Text style={styles.titleText}>Gr🍊cery List</Text>
+        </View>
+        <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Login')}
+      />
+        <TextInput
+          style={styles.todoInput}
+          value={input}
+          onChangeText={(text) => setInput(text)}
+        />
+        <Button
+          style={styles.appButtonText}
+          color="red"
+          title="Add To List"
+          onPress={addTodo}
+        />
+        <View>
+          {todos.map((todo) => (
+            <Todo style={styles.todoText} title={todo} />
+          ))}
+        </View>
+        {/* </ImageBackground> */}
+      </SafeAreaView>
+    
+  );
 };
 
 export default HomePage;
